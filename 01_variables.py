c1c0d9ee4539fc5da0bf98ef642ca1299ab1f4d9
Dante <danielbedoyalopez5@gmail.com>
2026-02-09 12:05:56 -0500

    feat: mi primer script de variables fCC

---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+# Definición de variables
+nombre_equipo = "Real Madrid"
+puntos = 53
+es_ganador = True
+
+# LA PARTE QUE FALTA: El print para mostrar los datos
+print("--- Reporte de Equipo ---")
+print(f"Equipo: {nombre_equipo}")
+print(f"Puntos actuales: {puntos}")
+print(f"¿Es líder?: {es_ganador}")
